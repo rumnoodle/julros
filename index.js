fbd756db8c3400f360750c0830eb502ba974ea26
Antero Karki <antero.nu@gmail.com>
2020-04-25 19:53:00 +0200

    fix some export error

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
-exports.render(options, callback) {
-    console.log(options);
-    console.log(callback);
-    return '<h1>Just testing</h1>';
-}
+exports.render = function (options, callback) {
+  console.log(options);
+  console.log(callback);
+  return "<h1>Just testing</h1>";
+};
