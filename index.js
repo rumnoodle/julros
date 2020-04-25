7c4fe6f55743aa13ff79b44f938126ed20277918
Antero Karki <antero.nu@gmail.com>
2020-04-25 20:10:23 +0200

    fix? express templating

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ exports.render = function (options, callback) {
 };
 
 exports.renderFile = function (path, options, callback) {
-  return exports.render(options, callback);
+  callback(null, exports.render(options, callback));
 };
 
 exports.__express = exports.renderFile;
