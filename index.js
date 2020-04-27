019fb27ba0b2c3c2ef426f9315c47998611ccfc5
Antero Karki <antero.nu@gmail.com>
2020-04-27 18:51:24 +0200

    add layout

--- index.js
+++ index.js
@@ -1,12 +1,42 @@
 const fs = require("fs");
+const path = require("path");
 
-exports.render = function (filename, options, callback) {
-  const content = fs.readFileSync(filename, "utf8");
+getLayout = function (filename) {
+  try {
+    return fs.readFileSync(filename, "utf8");
+  } catch (err) {
+    return "";
+  }
+};
+
+wrapLayout = function (layout, view) {
+  const viewRegex = /\{\{\s*view\s*\}\}/;
+  if (layout) {
+    if (layout.match(viewRegex)) {
+      return layout.replace(viewRegex, view);
+    }
+  }
+  return view;
+};
+
+exports.render = function (filepath, options) {
+  const relativeViewsFolder = options.settings.views.replace(/^\.\//, "");
+  const viewsFolder = filepath.substring(
+    0,
+    filepath.lastIndexOf(relativeViewsFolder) + relativeViewsFolder.length
+  );
+  const layoutsFolder = path.join(viewsFolder, "layouts");
+  const layoutFilename = (options.layout || "default") + ".julros";
+  const layoutFilepath = path.join(layoutsFolder, layoutFilename);
+  const layoutContent = getLayout(layoutFilepath);
+  ("");
+  const viewContent = fs.readFileSync(filepath, "utf8");
+  let content = wrapLayout(layoutContent, viewContent);
   return content;
 };
 
-exports.renderFile = function (filename, options, callback) {
-  callback(null, exports.render(filename, options, callback));
+exports.renderFile = function (filepath, options, callback) {
+  callback(null, exports.render(filepath, options));
 };
 
 exports.__express = exports.renderFile;
