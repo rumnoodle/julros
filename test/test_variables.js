9bbe9fb9437cfc30d5e76d72cc4d915f92efb2ee
Antero Karki <antero.nu@gmail.com>
2020-05-02 19:38:07 +0200

    add more variable features (tests only)

--- test/test_variables.js
+++ test/test_variables.js
@@ -14,4 +14,38 @@ describe("Test handling of variables", () => {
       replaceVariables(view, { variable: "value of variable" })
     );
   });
+
+  it("should handle multiple variables", () => {
+    const view = "<p>This is the view with { variable } and { another }</p>";
+    const expected =
+      "<p>This is the view with value of variable and woowoo</p>";
+
+    assert.equal(
+      expected,
+      replaceVariables(view, {
+        variable: "value of variable",
+        another: "woowoo",
+      })
+    );
+  });
+
+  it("should handle the same variable twice", () => {
+    const view = "<p>This is the view with { variable } and { variable }</p>";
+    const expected =
+      "<p>This is the view with value of variable and value of variable</p>";
+
+    assert.equal(
+      expected,
+      replaceVariables(view, {
+        variable: "value of variable",
+      })
+    );
+  });
+
+  it("should do nothing with variables that are undefined", () => {
+    const view = "<p>This is the view with { variable }</p>";
+    const expected = "<p>This is the view with { variable }</p>";
+
+    assert.equal(expected, replaceVariables(view, {}));
+  });
 });
