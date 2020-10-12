28e3a29318bce4605b6a7a7ec508bb5ec6a75452
Antero Karki <antero.nu@gmail.com>
2020-10-12 20:32:12 +0200

    parse variables

--- src/parser.js
+++ src/parser.js
@@ -1,9 +1,20 @@
 const pattern = /\{\{ .*? \}\}/;
 
-exports.html = (view) => {
-  while(item = pattern.exec(view)) {
-    // do something
+function getAttributeValue(path, data) {
+  if (!path.length) {
+    return data;
+  } else {
+    let current = path.shift();
+    return getAttributeValue(path, data[current]);
+  }
+}
+
+exports.html = (view, data) => {
+  let result = view;
+  while(item = pattern.exec(result)) {
+    let value = getAttributeValue(item[0].substring(2, item[0].length - 2).trim().split("."), data);
+    result = result.replace(pattern, value);
   }
 
-  return "";
+  return result;
 }
--- test/parser_test.js
+++ test/parser_test.js
@@ -6,4 +6,22 @@ describe("Test parser", () => {
     const result = parser.html("");
     assert.equal("", result);
   });
+
+  it("should return content as is if there isn't anything to replace", () => {
+    const content = "<p>Content should be returned as is";
+    const result = parser.html(content);
+    assert.equal(result, content);
+  });
+
+  it("should replace variable with its contents", () => {
+    const content = "<p>Violets are {{ violet }}</p>";
+    const result = parser.html(content, { violet: "blue" });
+    assert.equal(result, "<p>Violets are blue</p>");
+  });
+
+  it("should replace nested variable with its contents", () => {
+    const content = "<p>Paradise is {{ here.now }}</p>";
+    const result = parser.html(content, { here: { now: "a basket of flowers" } });
+    assert.equal(result, "<p>Paradise is a basket of flowers</p>");
+  });
 });
